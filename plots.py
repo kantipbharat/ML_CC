405e5c6e5a4f7fc8e08e54730ca6d649a98b5e19
Kanti Bharat <bharat@Kantis-MacBook-Pro.local>
2023-11-16 01:17:18 -0800

    plots update

--- plots.py
+++ plots.py
@@ -6,20 +6,32 @@ import os
 
 from helper import *
 
-dfs = [pickle.load(open('dataframes/' + filename, 'rb')) for filename in os.listdir('dataframes')]
+cols = ['cwnd', 'cwnd_order']
+cols += ['ewma_inter_send', 'min_inter_send']
+cols += ['ewma_inter_arr', 'min_inter_arr']
+cols += ['min_rtt']
+cols += ['ssthresh', 'throughput', 'max_throughput', 'loss_rate', 'overall_loss_rate', 'delay']
+cols += ['ratio_inter_send', 'ratio_inter_arr', 'ratio_rtt']
 
-for feature in dfs[0].columns:
+dfs = []
+for filename in os.listdir('dataframes'):
+    print(filename)
+    if filename.endswith('.pkl'):
+        df = pickle.load(open('dataframes/' + filename, 'rb'))
+        df = df.dropna(); df = df.reset_index(drop=True)
+        df.name = filename[:-4]; dfs.append(df)
+
+for feature in COLUMNS:
     fig, axes = plt.subplots(1, 2, figsize=(15, 5))
-    for df in dfs: axes[0].plot(df.index, df[feature], label=df.name)
+    for df in dfs: axes[0].plot(df.index, df[feature], label=feature)
     axes[0].set_title(f'{feature} over Time')
     axes[0].set_xlabel('Time')
     axes[0].set_ylabel(feature)
     axes[0].legend()
 
-    for df in dataframes: sns.kdeplot(df[feature], ax=axes[1], label=df.name)
+    for df in dfs: sns.kdeplot(df[feature], ax=axes[1], label=df.name)
     axes[1].set_title(f'Frequency Distribution of {feature}')
     axes[1].set_xlabel(feature)
     axes[1].set_ylabel('Density')
     axes[1].legend()
-
     plt.show()
