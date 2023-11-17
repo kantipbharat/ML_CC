2adfc196f24ff16ea985839c5ea2bb819082f3e2
Ramadevi Kanchadapu <ramadevikanchadapu@Ramadevis-MacBook-Pro.local>
2023-11-17 12:44:48 -0800

    plots finished

--- plots.py
+++ plots.py
@@ -3,45 +3,49 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import os
+import time
 
 from helper import *
 
-cols = [['cwnd', 'ssthresh']]
-cols += [['ewma_inter_send', 'ratio_inter_send']]
-cols += [['ewma_inter_arr', 'ratio_inter_arr']]
-cols += [['min_rtt', 'ratio_rtt']]
-cols += [['throughput', 'delay']]
-cols += [['loss_rate', 'overall_loss_rate']]
+cols = ['overall_loss_rate', 'overall_throughput', 'overall_delay']
+names = ['Loss Rate', 'Throughput', 'Delay']
 
 dfs = []
 for filename in os.listdir('data/dataframes'):
     if filename.endswith('.pkl'):
         df = pickle.load(open('data/dataframes/' + filename, 'rb'))
-        df = df.dropna(); df = df.reset_index(drop=True)
-        ranges = []; i = 0
 
-        for feature_set in cols:
-            for feature in feature_set:
-                first = np.percentile(df[feature], 1)
-                last = np.percentile(df[feature], 99)
-                ranges.append((first, last))
+        lost_sum = 0; delay_sum = 0; throughput = []; delay = []
+        received_list = list(df['recvd']); delay_list = list(df['delay'])
+        for i, t in enumerate(df['send_time']):
+            if received_list[i] == 0: lost_sum += 1
+            delay_sum += delay_list[i]
+            throughput.append(((i + 1) - lost_sum) / t)
+            delay.append(delay_sum / (i + 1))
         
-        for feature_set in cols:
-            for feature in feature_set:
-                df = df[(df[feature] >= ranges[i][0]) & (df[feature] <= ranges[i][1])]
-                df = df.reset_index(drop=True); i += 1
+        df['overall_throughput'] = throughput
+        df['overall_delay'] = delay
 
+        df = df.dropna(); df = df.reset_index(drop=True); ranges = []; i = 0
+        for feature in cols:
+            first = np.percentile(df[feature], 1)
+            last = np.percentile(df[feature], 99)
+            ranges.append((first, last))
+        
+        for feature in cols:
+            df = df[(df[feature] >= ranges[i][0]) & (df[feature] <= ranges[i][1])]
+            df = df.reset_index(drop=True); i += 1
+        
         df = df.reset_index(drop=True)
         df.name = filename[:-4]; dfs.append(df)
 
-for name, feature_set in enumerate(cols):
-    for feature in feature_set:
-        fig, ax = plt.subplots(nrows=1, ncols=len(feature_set), figsize=(12 * len(feature_set), 10))
-        for i, feature in enumerate(feature_set):
-            for df in dfs: 
-                ax[i].plot(df['send_time'], df[feature], label=df.name)
-            ax[i].set_title(f'{feature} over Time')
-            ax[i].set_xlabel('Time')
-            ax[i].set_ylabel(feature)
-            ax[i].legend(loc='upper right')
-        fig.savefig('data/figures/' + str(name + 1) + '.png')
+for i, feature in enumerate(cols):
+    fig, ax = plt.subplots(figsize=(12, 8))
+    for df in dfs: 
+        ax.plot(df['send_time'], df[feature], label=df.name)
+        ax.set_title(f'{names[i]} over Time')
+        ax.set_xlabel('Time')
+        ax.set_ylabel(names[i])
+        ax.legend(loc='upper right')
+    fig.savefig('data/figures/' + feature + '.png')
+    plt.close(fig)
